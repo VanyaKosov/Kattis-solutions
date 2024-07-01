44174c8555578cf6a548a49d636270c82312c29f
Ivan Kosov <kosov.vanya@gmail.com>
2024-07-01 10:36:36 -0500

    Renamed a few files

---
+++ Gamerank/Main.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+public class Main {
+    private static final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+
+        writer.close();
+    }
+}
