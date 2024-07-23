479bdf004852f8f0f21297b3aa47e228ac436020
Ivan Kosov <kosov.vanya@gmail.com>
2024-07-23 15:38:11 -0500

    Updated template

--- _Template/Main.java
+++ _Template/Main.java
@@ -3,11 +3,11 @@ import java.util.*;
 import java.lang.*;
 
 public class Main {
-    private static final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-    private static final BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static final BufferedReader READER = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter WRITER = new BufferedWriter(new OutputStreamWriter(System.out));
 
     public static void main(String[] args) throws IOException {
 
-        writer.close();
+        WRITER.close();
     }
 }
