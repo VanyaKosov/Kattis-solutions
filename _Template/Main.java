a9582302a43fe38a07a57062ac0862bd7c2e2ab1
Ivan Kosov <kosov.vanya@gmail.com>
2024-11-10 18:04:15 -0600

    Updated template (added 10mB buffer)

--- _Template/Main.java
+++ _Template/Main.java
@@ -3,8 +3,8 @@ import java.util.*;
 import java.lang.*;
 
 public class Main {
-    private static final BufferedReader READER = new BufferedReader(new InputStreamReader(System.in));
-    private static final BufferedWriter WRITER = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static final BufferedReader READER = new BufferedReader(new InputStreamReader(System.in), 10000000);
+    private static final BufferedWriter WRITER = new BufferedWriter(new OutputStreamWriter(System.out), 10000000);
 
     public static void main(String[] args) throws IOException {
 
